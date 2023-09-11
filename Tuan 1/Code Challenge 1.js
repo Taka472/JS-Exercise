862bb193b937bb4aa73142f331e4ec7a14711a9b
unknown <tranlehuy0407@gmail.com>
2023-09-11 13:30:44 +0700

    second commit

--- Tuan 1/Code Challenge 1.js
+++ Tuan 1/Code Challenge 1.js
@@ -1,3 +1,4 @@
+//20127201 - Tran Le Huy
 let BMI1, BMI2;
 
 function BMICal(mass, height) {
@@ -8,7 +9,7 @@ function markHigherBMI(mass1, height1, mass2, height2) {
     BMI1 = BMICal(mass1, height1);
     BMI2 = BMICal(mass2, height2);
     if (BMI1 > BMI2) return true;
-    return false
+    return false;
 }
 
-console.log(markHigherBMI());
+console.log(markHigherBMI(78, 1.69, 92, 1.95));
