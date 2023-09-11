61f6da616f828bb24fbcc425ace51918175e7e87
unknown <tranlehuy0407@gmail.com>
2023-09-11 13:13:51 +0700

    first commit

---
+++ Tuan1.js
@@ -0,0 +1,14 @@
+let BMI1, BMI2;
+
+function BMICal(mass, height) {
+    return mass / (height * height);
+}
+
+function markHigherBMI(mass1, height1, mass2, height2) {
+    BMI1 = BMICal(mass1, height1);
+    BMI2 = BMICal(mass2, height2);
+    if (BMI1 > BMI2) return true;
+    return false
+}
+
+console.log(markHigherBMI());
