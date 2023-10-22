fdd7b97b8123f50a590ab1fb886985b88663c2bf
Huy Trần Lê <tranlehuy0407@gmail.com>
2023-10-22 09:52:40 +0700

    sixth week commit

---
+++ STT_07TranLeHuy06/App.js
@@ -0,0 +1,13 @@
+
+import Screen1 from './Pictures/pictures1';
+import Screen2 from './Pictures/pictures2';
+export default function App() {
+  return (
+    <>
+    <Screen2/>
+  </>
+    
+  );
+}
+
+
