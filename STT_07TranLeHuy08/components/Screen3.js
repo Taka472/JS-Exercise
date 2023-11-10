f18cc28ce6bad3e2667ff9d03484d5533e91c5a9
Huy Trần Lê <tranlehuy0407@gmail.com>
2023-11-10 23:10:26 +0700

    eighth week commit

--- STT_07TranLeHuy08/components/Screen3.js
+++ STT_07TranLeHuy08/components/Screen3.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
+import { ScrollView } from 'react-native-web';
 
 export default function App({navigation, route}) {
-  const [data, setData] = useState([])
+  const [DATA, setData] = useState([])
+
+  const navigateToScreen4 = (id) => {
+    console.log(id);
+  }
 
   useEffect(() => {
       fetch("https://653f489e9e8bd3be29e02887.mockapi.io/account/" + route.params + "/note")
@@ -11,15 +16,34 @@ export default function App({navigation, route}) {
         .then((json) => setData(json))
   }, [])
 
-  const Item = ({item}) => (
-    <TouchableOpacity>
-        <View></View>
+  const Item = ({items}) => (
+    <TouchableOpacity onPress={(navigateToScreen4(items.id))}>
+        <View style={{width: 130, height: 130, borderWidth: 1, margin: 15, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'}}>
+          <Text>{items.header}</Text>
+        </View>
     </TouchableOpacity>
-  )
+  )  
 
   return (
     <View style={styles.container}>
-      <Text>Screen 3</Text>
+      <View>
+        <Text style={{fontSize: 25, fontWeight: 'bold'}}>Welcome to your notebook</Text>
+      </View>
+      <ScrollView>
+        <FlatList
+          data={DATA}
+          numColumns={2}
+          renderItem={({item}) => <Item items={item}></Item>}
+          keyRender={(item) => item.id}
+        ></FlatList>
+      </ScrollView>
+      <View>
+        <TouchableOpacity>
+          <View style={{backgroundColor: 'red', width: 50, height: 50, marginBottom: 20, borderRadius: '50%', justifyContent: 'center', alignItems: 'center', marginLeft: 300}}>
+            <Text style={{fontSize: 30, color: 'white', paddingBottom: 8}}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -27,7 +51,7 @@ export default function App({navigation, route}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F1F1F1',
     alignItems: 'center',
     justifyContent: 'flex-start',
     paddingTop: 20,
